c1ddf70347185dffd257901cb1b2d6ac51999a19
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-12-04 14:44:16 -0300

    more things

--- omni_otus.mk
+++ omni_otus.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/motorola/otus/full_otus.mk)
+$(call inherit-product, device/microsoft/fh/full_fh.mk)
 
-PRODUCT_NAME := omni_otus
+PRODUCT_NAME := omni_fh
