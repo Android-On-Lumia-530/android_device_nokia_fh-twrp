bf63aa09e7ac357bd3b2d6f6d784122bd14c5f5f
Vicc2008 <66806243+Vicc2008@users.noreply.github.com>
2021-10-09 23:05:56 -0300

    change the Microsoft to Nokia

--- omni_fh.mk
+++ omni_fh.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/microsoft/fh/full_fh.mk)
+$(call inherit-product, device/nokia/fh/full_fh.mk)
 
 PRODUCT_NAME := omni_fh
